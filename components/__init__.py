fa755134e10871d52d5f8b9658899b3ab5abe0d0
Antony-M1 <antony.praveenkumar@softsuave.com>
2024-10-23 10:48:42 +0530

    function configure

---
+++ components/__init__.py
@@ -0,0 +1,9 @@
+from .article import create_article_form, create_article_list
+from .llm import create_ai_suggestion
+
+
+__all__ = [
+    'create_article_form',
+    'create_article_list',
+    'create_ai_suggestion',
+]
