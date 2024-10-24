5419d4fcbf33d43b76ade749b70870859fa73e31
Antony-M1 <antony.praveenkumar@softsuave.com>
2024-10-24 16:13:40 +0530

    Main file update

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from dotenv import load_dotenv
 import streamlit as st
 from models import Article, create_engine_session
+from post_bot import PostBot
 from components import (
     create_article_form,
     create_article_list,
-    create_ai_suggestion
+    create_ai_suggestion,
+    create_script_run
 )
 
 
@@ -15,7 +17,7 @@ db_session = Session()
 
 st.title('LinkedIn Post Bot')
 
-tab_article, tab_article_list, tab_ai, tab_dash = st.tabs(["📝 Article", "🗃 List of Articles", "🤖Gemini AI", "📈 Dashboard"]) # noqa
+tab_article, tab_article_list, tab_ai, tab_run = st.tabs(["📝 Article", "🗃 List of Articles", "🤖Gemini AI", "🏃 Run"]) # noqa
 
 with tab_article:
     create_article_form(Article, db_session)
@@ -25,3 +27,6 @@ with tab_article_list:
 
 with tab_ai:
     create_ai_suggestion(generator=tab_ai)
+
+with tab_run:
+    create_script_run(PostBot, generator=tab_run)
